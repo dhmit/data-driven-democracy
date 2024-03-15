fb7f2dde26e6bff952c11afd07f9860043524d33
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-03-15 15:08:19 -0400

    Added MapBase and Legend

---
+++ frontend/components/global/Legend.js
@@ -0,0 +1,35 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import "../../scss/legend.scss";
+
+
+export default function Legend({
+    layers,
+    visibleLayers,
+    toggleLayer
+}) {
+    
+    return (
+        <div className="legend">
+            <ul>
+                {layers.map(layer => {
+                    return <li key={layer}>
+                        <button
+                            value={layer}
+                            onClick={(e) => toggleLayer(e)}
+                            className={`legend-square
+                            ${layer.toLowerCase()}
+                            ${visibleLayers.indexOf(layer) > -1 ? "" : "inactive"}`}/>
+                        <span className={"label"}>{layer}</span>
+                    </li>;
+                })}
+            </ul>
+        </div>
+    );
+}
+
+Legend.propTypes = {
+    layers: PropTypes.array,
+    visibleLayers: PropTypes.array,
+    toggleLayer: PropTypes.func
+};
